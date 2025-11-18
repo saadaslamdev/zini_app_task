04c546d1a9d9789dc7eccbff95d9fbe23d114799
Saad Aslam <saadaslam.graphics@gmail.com>
2025-11-18 17:46:37 +0500

    Test commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,6 +11,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:zini_pay_task/src/my_app.dart';
 
 void main() {
+
+  
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
